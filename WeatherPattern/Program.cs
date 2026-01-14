8da493f8d85615f8522afbd504dee0d0960a488d
Juan Oh <joh@restaurant365.com>
2026-01-14 20:04:31 -0300

    feat: refactor Program.cs to delegate user interaction to ConsoleMenu
    Moved user input and file selection logic to Presentation/ConsoleMenu.cs
    Simplified Program.cs to improve maintainability and adhere to SOLID principles
    Prepared for easier extension and testing

--- WeatherPattern/Program.cs
+++ WeatherPattern/Program.cs
@@ -1,36 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using WeatherPattern.Event;
-using WeatherPattern.Models;
-using WeatherPattern.Readers;
-using WeatherPattern.Utils;
+using WeatherPattern.Presentation;
 
-List<Weather> weather;
-
-while (true)
-{
-    try
-    {
-        Console.WriteLine("Input weather file: (input.json - input.xml)");
-        var choice = Console.ReadLine();
-
-        if (choice is null) continue;
-        Console.WriteLine($"Reading {choice}");
-        weather = await TypeSelector.ReadAsync(PathParser.GetPath(choice));
-            
-        Console.WriteLine($"Found {weather.Count} weather entries");
-        if (weather.Count > 0) break;
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex.Message);
-    }
-}
-        
-var botReader = new BotReader();
-var bots = await botReader.ReadAsync(PathParser.GetPath("bots.json"));
-        
-Console.WriteLine($"Found {bots.Count} bots");
+var (weather, bots) = await ConsoleMenu.GetWeatherAndBotsAsync();
 
 var manager = new EventManager(weather, bots);
 manager.Report();
